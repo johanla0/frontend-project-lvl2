528d5dbf3a299c519e4d4ca4d29f1b200fd65d54
Ivan Dolgov <ivandolgov@setyl.com>
2020-12-20 15:53:30 +0300

    Step 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const run = () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
-    .helpOption('-h, --help', 'output usage information');
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
   program.parse(process.argv);
 
   // program.option('-d, --debug', 'output extra debugging')
