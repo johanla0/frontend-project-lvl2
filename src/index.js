c1f2de0392483a6c0cecde0ebd0420ca0001809b
Ivan Dolgov <ivandolgov@setyl.com>
2021-01-11 12:43:55 +0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import render from './formatters/index.js';
 
 const getFileExtension = (filepath) => path.extname(filepath).slice(1);
 const readFile = (filepath) => fs.readFileSync(path.resolve(filepath), 'utf-8');
+const getData = (filepath) => parse(readFile(filepath), getFileExtension(filepath));
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
-  const type1 = getFileExtension(filepath1);
-  const type2 = getFileExtension(filepath2);
-  const oldObject = parse(file1, type1);
-  const newObject = parse(file2, type2);
+  const oldObject = getData(filepath1);
+  const newObject = getData(filepath2);
   const tree = buildTree(oldObject, newObject);
   return render(tree, format);
 };
