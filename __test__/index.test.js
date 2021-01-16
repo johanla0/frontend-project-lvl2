82cef153834f5a5c13e67da60bb1dee281734ec6
Ivan Dolgov <ivandolgov@setyl.com>
2021-01-16 20:16:03 +0300

    Refactor

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -10,19 +10,24 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each([
-  ['json', 'stylish'],
-  ['yml', 'stylish'],
-  ['json', 'plain'],
-  ['yml', 'plain'],
-  ['json', 'json'],
-  ['yml', 'json'],
-])('%s files format as %s', (extension, format) => {
+const stylishResult = readFile('stylish_expected').trim();
+const plainResult = readFile('plain_expected').trim();
+const jsonResult = readFile('json_expected').trim();
+
+test.each(['yml', 'json'])('gendiff %s', (extension) => {
   const file1 = getFixturePath(`nested_file1.${extension}`);
   const file2 = getFixturePath(`nested_file2.${extension}`);
-  const data = readFile(`${format}_expected`);
-  const expected = data.trim();
-  expect(gendiff(file1, file2, format)).toEqual(expected);
+  expect(gendiff(file1, file2)).toEqual(stylishResult);
+  expect(gendiff(file1, file2, 'stylish')).toEqual(stylishResult);
+  expect(gendiff(file1, file2, 'plain')).toEqual(plainResult);
+  expect(gendiff(file1, file2, 'json')).toEqual(jsonResult);
+});
+
+test('json is correct', () => {
+  const file1 = getFixturePath('nested_file1.json');
+  const file2 = getFixturePath('nested_file2.json');
+  const data = gendiff(file1, file2, 'json');
+  expect(() => JSON.parse(data)).not.toThrow();
 });
 
 test('parse throws unsupported error', () => {
