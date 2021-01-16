5bb325ec031cc9f302fd905c9b6cec47bd28fe0e
Ivan Dolgov <ivandolgov@setyl.com>
2021-01-16 19:54:41 +0300

    Refactor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
-import run from '../src/commander.js';
+import program from 'commander';
+import gendiff from '../index.js';
 
-run();
+const run = () => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+    .helpOption('-h, --help', 'render usage information')
+    .option(
+      '-f, --format [type]',
+      'render format: stylish / plain / json',
+      'stylish',
+    )
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      try {
+        console.info(gendiff(filepath1, filepath2, program.format));
+      } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+    });
+  program.parse(process.argv);
+};
+
+export default run;
