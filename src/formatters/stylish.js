c6e3fe7fe240729a3dab4e6fbbda0f95d29d07e2
Ivan Dolgov <ivandolgov@setyl.com>
2021-01-16 18:21:56 +0300

    Refactor

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,7 +6,6 @@ const types = {
   added: '  + ',
   removed: '  - ',
   unchanged: '    ',
-  nested: '    ',
 };
 
 const indent = (depth) => ' '.repeat(depth * numOfSpaces);
@@ -21,38 +20,36 @@ const stringify = (record, depth) => {
   return ['{', ...entries, `${indent(depth)}}`].join('\n');
 };
 
-const formatProperty = (depth, type, propertyName) => `${indent(depth - 1)}${types[type]}${propertyName}`;
-
 const stylish = (tree) => {
   const represent = (obj, depth = 1) => obj.flatMap(({
     propertyName, children, oldValue, newValue, type,
   }) => {
     switch (type) {
       case 'added':
-        return `${formatProperty(depth, type, propertyName)}: ${stringify(
+        return `${indent(depth - 1)}${types[type]}${propertyName}: ${stringify(
           newValue,
           depth,
         )}`;
       case 'removed':
       case 'unchanged':
-        return `${formatProperty(depth, type, propertyName)}: ${stringify(
+        return `${indent(depth - 1)}${types[type]}${propertyName}: ${stringify(
           oldValue,
           depth,
         )}`;
       case 'updated':
         return [
-          `${formatProperty(depth, 'removed', propertyName)}: ${stringify(
+          `${indent(depth - 1)}${types.removed}${propertyName}: ${stringify(
             oldValue,
             depth,
           )}`,
-          `${formatProperty(depth, 'added', propertyName)}: ${stringify(
+          `${indent(depth - 1)}${types.added}${propertyName}: ${stringify(
             newValue,
             depth,
           )}`,
         ];
       case 'nested':
         return [
-          `${formatProperty(depth, type, propertyName)}: {`,
+          `${indent(depth)}${propertyName}: {`,
           ...represent(children, depth + 1),
           `${indent(depth)}}`,
         ];
--- src/tree.js
+++ src/tree.js
@@ -5,17 +5,42 @@ const buildTree = (oldObject, newObject) => {
   const newProperties = Object.keys(newObject);
   const properties = _.sortBy(_.union(oldProperties, newProperties));
   const compare = (property) => {
-    if (!_.has(oldObject, property)) { return { propertyName: property, newValue: newObject[property], type: 'added' }; }
-    if (!_.has(newObject, property)) { return { propertyName: property, oldValue: oldObject[property], type: 'removed' }; }
-    if (_.isPlainObject(oldObject[property])
-      && _.isPlainObject(newObject[property])
-    ) { return { propertyName: property, children: buildTree(oldObject[property], newObject[property]), type: 'nested' }; }
-    if (oldObject[property] !== newObject[property]) {
+    if (!_.has(oldObject, property)) {
       return {
-        propertyName: property, oldValue: oldObject[property], newValue: newObject[property], type: 'updated',
+        propertyName: property,
+        newValue: newObject[property],
+        type: 'added',
       };
     }
-    return { propertyName: property, oldValue: oldObject[property], type: 'unchanged' };
+    if (!_.has(newObject, property)) {
+      return {
+        propertyName: property,
+        oldValue: oldObject[property],
+        type: 'removed',
+      };
+    }
+    if (_.isPlainObject(oldObject[property])
+      && _.isPlainObject(newObject[property])
+    ) {
+      return {
+        propertyName: property,
+        children: buildTree(oldObject[property], newObject[property]),
+        type: 'nested',
+      };
+    }
+    if (oldObject[property] !== newObject[property]) {
+      return {
+        propertyName: property,
+        oldValue: oldObject[property],
+        newValue: newObject[property],
+        type: 'updated',
+      };
+    }
+    return {
+      propertyName: property,
+      oldValue: oldObject[property],
+      type: 'unchanged',
+    };
   };
   return properties.map(compare);
 };
